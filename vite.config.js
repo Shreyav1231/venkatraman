573a3b893e7eb454b7e914223828ab848ed93e62
Shreyav1231 <shreyav1231@gmail.com>
2026-08-21 11:06:20 -0400

    final consistency maintained

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// making base dynamic for local testing as well
+const base = process.env.NODE_ENV === 'production' ? '/venkatraman/' : '/';
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base,
 })
