331a5f9faf2e20aea3b0ca8f557e0a9909c48a62
Shreyav1231 <shreyav1231@gmail.com>
2026-08-29 16:15:47 -0400

    root changed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// making base dynamic for local testing as well
-const base = process.env.NODE_ENV === 'production' ? '/venkatraman/' : '/';
+const base = process.env.SITE_BASE ?? '/';
 export default defineConfig({
   plugins: [react()],
   base,
